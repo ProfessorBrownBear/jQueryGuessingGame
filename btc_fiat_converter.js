8f55549c330d67a94921dd41bc12a4d37a0c9623
Peter Sigurdson <139134226+ProfessorBrownBear@users.noreply.github.com>
2023-07-26 14:18:52 -0400

    Create btc_fiat_converter.js

---
+++ btc_fiat_converter.js
@@ -0,0 +1,91 @@
+To create a web app that converts between Bitcoin and various currencies using HTML, CSS, and JavaScript, you can follow these steps:
+Step 1: Set up the HTML structure
+Create an index.html file with the following structure:
+html
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Bitcoin Converter</title>
+  <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+  <h1>Bitcoin Converter</h1>
+  <form id="converter-form">
+    <input type="number" id="amount" placeholder="Amount">
+    <select id="currency">
+      <option value="USD">USD</option>
+      <option value="EUR">EUR</option>
+      <!-- Add more currencies here -->
+    </select>
+    <button type="submit">Convert</button>
+  </form>
+  <div id="result"></div>
+  <script src="app.js"></script>
+</body>
+</html>
+
+Step 2: Style the web app with CSS
+Create a styles.css file and add your desired styles. For example:
+css
+body {
+  font-family: Arial, sans-serif;
+  text-align: center;
+}
+
+h1 {
+  margin-bottom: 20px;
+}
+
+#converter-form {
+  display: inline-flex;
+  gap: 10px;
+}
+
+#result {
+  margin-top: 20px;
+}
+
+Step 3: Implement the JavaScript logic
+Create an app.js file and use the following code to fetch the conversion rate from a public API, such as CoinAPI.io
+17
+, and perform the conversion:
+javascript
+const form = document.getElementById('converter-form');
+const amountInput = document.getElementById('amount');
+const currencySelect = document.getElementById('currency');
+const resultDiv = document.getElementById('result');
+
+form.addEventListener('submit', async (event) => {
+  event.preventDefault();
+  const amount = parseFloat(amountInput.value);
+  const currency = currencySelect.value;
+
+  if (isNaN(amount)) {
+    alert('Please enter a valid amount');
+    return;
+  }
+
+  const apiKey = 'YOUR_API_KEY'; // Replace with your CoinAPI.io API key
+  const url = `https://rest.coinapi.io/v1/exchangerate/BTC/${currency}?apikey=${apiKey}`;
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    if (response.ok) {
+      const convertedAmount = amount * data.rate;
+      resultDiv.textContent = `${amount} BTC is equal to ${convertedAmount.toFixed(2)} ${currency}`;
+    } else {
+      alert(`Error: ${data.error}`);
+    }
+  } catch (error) {
+    alert('Failed to fetch conversion rate');
+  }
+});
+
+Replace 'YOUR_API_KEY' with your actual API key from CoinAPI.io
+17
+. You can sign up for a free API key on their website.
+Now, you have a simple web app that converts between Bitcoin and various currencies using HTML, CSS, and JavaScript. You can add more currencies to the select element in the HTML file as needed.
